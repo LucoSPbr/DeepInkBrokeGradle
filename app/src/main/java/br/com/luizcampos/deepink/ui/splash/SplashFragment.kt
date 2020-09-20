95c002a110f7b0fa93fc1a4195242eb0236e627a
Luiz <luco13sp@gmail.com>
2020-09-20 18:27:12 -0300

    Abertura.

---
+++ app/src/main/java/br/com/luizcampos/deepink/ui/splash/SplashFragment.kt
@@ -0,0 +1,39 @@
+package br.com.luizcampos.deepink.ui.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import br.com.luizcampos.deepink.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SplashFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_splashFragment_to_login_nav_graph)
+        }, 2000)
+    }
+}
