7e6abba309b3a42395679175cc5b5a005513a327
Luiz <luco13sp@gmail.com>
2020-09-20 19:21:05 -0300

    Adições.

--- app/src/main/java/br/com/luizcampos/deepink/ui/splash/SplashFragment.kt
+++ app/src/main/java/br/com/luizcampos/deepink/ui/splash/SplashFragment.kt
@@ -6,19 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.NavHostFragment
 import br.com.luizcampos.deepink.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashFragment : Fragment() {
 
     override fun onCreateView(
@@ -32,8 +24,13 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Handler().postDelayed({
+            val extras = FragmentNavigatorExtras(
+                ivLogoApp to "logoApp",
+                tvAppName to "textApp"
+            )
             NavHostFragment.findNavController(this)
-                .navigate(R.id.action_splashFragment_to_login_nav_graph)
+                .navigate(R.id.action_splashFragment_to_login_nav_graph,
+                null, null, extras)
         }, 2000)
     }
 }
